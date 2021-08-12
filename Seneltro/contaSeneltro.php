64edd7582f4c1f7444658e99da093298ca72609a
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-08-12 07:50:42 -0300

    Atividade Finalizada

---
+++ Seneltro/contaSeneltro.php
@@ -0,0 +1,49 @@
+<?php
+
+if (
+    isset($_POST["nome"]) && isset($_POST["endereco"])
+    && isset($_POST["consumo"]))
+ {
+
+    $nome = $_POST["nome"];
+    $endereco = $_POST["endereco"];
+    $consumo = $_POST["consumo"];
+
+    $valorAPagar = 0;
+
+    if ($consumo >= 120) {
+        $valorAPagar = $consumo * 0.42;
+        $status = "alto";
+    }
+    else{
+        $valorAPagar = $consumo * 0.32;
+        $status = "baixo";
+        echo "
+        Obrigada por economizar :)
+         <br> <br> <br> <br>";
+    }
+
+}
+else{
+    echo "Você não preencheu coretamente os campos";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Resultado</title>
+    <link href="styles.css" rel="stylesheet" />
+</head>
+<body>
+    <h1 class="titulo">Conta de luz de <?=$nome?> </h1> <br>
+    <h2 class="endereço"><?=$endereco?>. </h2> <br>
+    <h2 class="<?= $status ?>"> Consumo: <?=$consumo?> kWh.</h2> <br>
+    <h2 class="valor">Valor a pagar: <h1 class="valor">R$ <?=number_format($valorAPagar, 2, "," , ".")?>.</h1></h2>
+</body>
+</html>
